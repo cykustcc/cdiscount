e224341051d473b24beecd23ada14122d32e5043
Yukun Chen <cykustc@gmail.com>
2017-09-28 22:19:07 -0400

    add imgs_size_stat.

--- src/read_data.py
+++ src/read_data.py
@@ -1,3 +1,8 @@
+r"""
+A set of utils to do data preprocessing and get basic statistics.
+"""
+from google.apputils import app
+import gflags
 import numpy as np
 import pandas as pd
 import io
@@ -7,6 +12,16 @@ import csv
 from skimage.data import imread   # or, whatever image library you prefer
 import multiprocessing as mp      # will come in handy due to the size of the data
 
+FLAGS = gflags.FLAGS
+
+gflags.DEFINE_bool('loop_and_see_example', False,
+                   'Loop through train_example.bson and see images.')
+
+gflags.DEFINE_bool('get_img_cnt_hist', False,
+                   'Get image count per product histogram.')
+
+gflags.DEFINE_bool('get_img_size_stat', False,
+                   'Calculate mean and std of image width and height.')
 
 def loop_and_see(bson_file, show_img=False):
   """
@@ -20,7 +35,7 @@ def loop_and_see(bson_file, show_img=False):
       cnt = 0;
       for e, pic in enumerate(d['imgs']):
         if show_img:
-          picture = imread(io.BytesIO(pic['picture']))
+          picture = np.array(imread(io.BytesIO(pic['picture'])))
           # do something with the picture, etc
           plt.imshow(picture);
           plt.pause(5)
@@ -31,7 +46,8 @@ def loop_and_see(bson_file, show_img=False):
         num_of_imgs_hist[cnt] = 1
   return num_of_imgs_hist
 
-def get_img_cnt_for_a_prodcuct_histogram(bson_file,
+
+def get_img_cnt_per_prodcuct_histogram(bson_file,
     stat_file='../data/img_cnt_for_a_prodcuct_histogram.txt'):
   num_of_imgs_hist = loop_and_see(bson_file)
   print num_of_imgs_hist
@@ -41,7 +57,38 @@ def get_img_cnt_for_a_prodcuct_histogram(bson_file,
       writer.writerow([k, d])
 
 
+def imgs_size_stat(bson_file,
+    stat_file='../data/img_size_stat.txt'):
+  data = bson.decode_file_iter(open(bson_file, 'rb'))
+  num_of_imgs_hist = {}
+  # num_of_imgs = 4369441 + 1128588 * 2 + 542792 * 3 + 1029075 * 4 
+  num_of_imgs = 12371293
+  widths = num_of_imgs * [0]
+  heights = num_of_imgs * [0]
+  # widths = num_of_imgs * [None]
+  # heights = num_of_imgs * [None]
+  idx = 0;
+  for c, d in enumerate(data):
+    for e, pic in enumerate(d['imgs']):
+        picture = np.array(imread(io.BytesIO(pic['picture'])))
+        [w, h, c] = picture.shape
+        widths[idx] = w
+        heights[idx] = h
+  width_mean = np.mean(widths);
+  width_std = np.std(widths);
+  height_mean = np.mean(heights);
+  height_std = np.std(heights);
+  with open(stat_file, 'wb') as f:
+    writer = csv.writer(f)
+    writer.writerow([width_mean, width_std, height_mean, height_std])
+
+def main(argv):
+  if FLAGS.loop_and_see_example:
+    loop_and_see('../data/train_example.bson', show_img=True)
+  if FLAGS.get_img_cnt_hist:
+    get_img_cnt_per_prodcuct_histogram('../data/train_example.bson')
+  if FLAGS.get_img_size_stat:
+    imgs_size_stat('../data/train_example.bson')
 
 if __name__ == '__main__':
-  # loop_and_see('../data/train_example.bson', show_img=False)
-  get_img_cnt_for_a_prodcuct_histogram('../data/train.bson')
+  app.run()
