45a3e2e24dcf26f2005d7b769ab97afec9e9b074
Yukun Chen <cykustc@gmail.com>
2017-10-08 17:25:03 -0400

    add train validation split.

--- src/cdiscount_data.py
+++ src/cdiscount_data.py
@@ -2,12 +2,30 @@
 # -*- coding: utf-8 -*-
 # File: cdiscount_data.py
 # Author: Yukun Chen <cykustc@gmail.com>
+from google.apputils import app
+import gflags
 import os
 from tqdm import tqdm
 import numpy as np
 import cv2
 
 from tensorpack.dataflow.base import RNGDataFlow
+from tensorpack.dataflow import *
+
+FLAGS = gflags.FLAGS
+
+gflags.DEFINE_bool('get_per_pixel_mean_img', False,
+                   'Calculate per pixel mean image.')
+
+gflags.DEFINE_bool('loop_and_see_example_imgs', False,
+                   'Loop and view example images.')
+
+gflags.DEFINE_bool('test_batch_data', False,
+                   'Test batch read mode speed.')
+
+gflags.DEFINE_bool('create_lmdb', False,
+                   'Create lmdb for training set for sequential read.')
+
 
 __all__ = ['Cdiscount']
 
@@ -23,10 +41,10 @@ class Cdiscount(RNGDataFlow):
       name (str): 'train' or 'test'.
     """
     assert os.path.isdir(dir), dir
-    assert name in ['train', 'test']
+    assert name in ['train', 'val', 'test']
     self.full_path = dir
     self.filepath_label_file = filepath_label_file
-    self.imglist = self._get_img_list()
+    self.imglist = self._get_img_list(name)
     if shuffle == None:
       shuffle = name == 'train'
     self.shuffle = shuffle
@@ -35,7 +53,15 @@ class Cdiscount(RNGDataFlow):
       fname = os.path.join(self.full_path, fname)
       assert os.path.isfile(fname), fname
 
-  def _get_img_list(self):
+  def _get_train_val_split(self, total_num_imgs):
+    """Split the orginal training set to train and validation set. (0.8, 0.2)
+      Return: a list of [True, False,...]
+      True is training set datapoint and False is validation set datapoint.
+    """
+    np.random.seed(0)
+    return list(np.greater(np.random.random(total_num_imgs), 0.2))
+
+  def _get_img_list(self, train_or_val_or_test):
     with open(self.filepath_label_file) as f:
       ret = []
       for line in f.readlines():
@@ -43,7 +69,13 @@ class Cdiscount(RNGDataFlow):
         cls = int(cls)
         ret.append([name, cls])
     assert len(ret)
-    return ret
+    train_val_split = self._get_train_val_split(len(ret))
+    if train_or_val_or_test == 'train':
+      return [ret[i] for i in xrange(len(ret)) if train_val_split[i]]
+    elif train_or_val_or_test == 'val':
+      return [ret[i] for i in xrange(len(ret)) if not train_val_split[i]]
+    else:
+      return ret
 
   def get_data(self):
     for fname, label in self.get_filename_label():
@@ -97,12 +129,39 @@ class Cdiscount(RNGDataFlow):
       if cnt >= num:
         break
 
+def main(argv):
+  if FLAGS.loop_and_see_example_imgs:
+    ds = Cdiscount('../data/train_imgs_example',
+                   '../data/train_imgfilelist_example.txt',
+                   'train')
+    ds.reset_state()
+    print len(ds._get_img_list('train'))
+    print len(ds._get_img_list('val'))
+    ds.loop_and_see_example_imgs(20)
+  if FLAGS.get_per_pixel_mean_img:
+    ds = Cdiscount('../data/train_imgs',
+                   '../data/train_imgfilelist.txt',
+                   'train')
+    ds.reset_state()
+    ds.get_per_pixel_mean()
+  if FLAGS.test_batch_data:
+    ds = Cdiscount('../data/train_imgs',
+                   '../data/train_imgfilelist.txt',
+                   'train')
+    ds.reset_state()
+    ds1 = BatchData(ds, 256, use_list=True)
+    TestDataSpeed(ds1).start()
+  if FLAGS.create_lmdb:
+    ds = Cdiscount('../data/train_imgs',
+                   '../data/train_imgfilelist.txt',
+                   'train')
+    ds.reset_state()
+    ds1 = PrefetchDataZMQ(ds, nr_proc=1)
+    dftools.dump_dataflow_to_lmdb(ds1, '../data/Cdiscount-train.lmdb')
+
+
+
+
 if __name__ == '__main__':
-  ds = Cdiscount('../data/train_imgs',
-                 '../data/train_imgfilelist.txt',
-                 'train')
-  ds.reset_state()
-  #print ds._get_img_list()
-  #ds.loop_and_see_example_imgs(20)
-  ds.get_per_pixel_mean()
+  app.run()
 
